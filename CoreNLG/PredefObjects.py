3b16f3a6bd6ea385bcaa31dffd6031e491fbc55a
Fabien Giely <fabien.giely-ext@socgen.com>
2019-12-09 11:01:09 +0100

    move predefine objects to corresponding object

--- CoreNLG/PredefObjects.py
+++ CoreNLG/PredefObjects.py
@@ -4,53 +4,6 @@ created on 08/01/2019 10:21
 @author: fgiely
 """
 
-interpretable_char = {
-    ".": "DOT",
-    ",": "COMA",
-    '"': "QUOTE",
-    "?": "INTERROGATION",
-    "!": "EXCLAMATION",
-    ":": "DOUBLEDOTS",
-    "/": "SLASH",
-    " ": "SPACE",
-    ";": "SEMICOLON",
-    "&": "AMPERSAND",
-    "(": "OPENING_PARENTHESIS",
-    ")": "CLOSING_PARENTHESIS",
-    "<": "LESS_THAN",
-    ">": "MORE_THAN",
-    "[": "OPENING_BRACKET",
-    "]": "CLOSING_BRACKET",
-    "\n": "CARRIAGE_RETURN",
-}
-
-interpretable_char_reverse = {value: key for key, value in interpretable_char.items()}
-
-class IteratorConstructor:
-    def __init__(
-            self,
-            max_elem=None,
-            nb_elem_bullet=None,
-            begin_w="",
-            end_w="",
-            sep=",",
-            last_sep="et",
-            capitalize_bullets=True,
-            text_if_empty_list="",
-            end_of_bullet="",
-            end_of_last_bullet=""
-    ):
-        self.max_elem = max_elem
-        self.nb_elem_bullet = nb_elem_bullet
-        self.begin_w = begin_w
-        self.end_w = end_w
-        self.sep = sep
-        self.last_sep = last_sep
-        self.capitalize_bullets = capitalize_bullets
-        self.text_empty_list = text_if_empty_list
-        self.end_of_bullet = end_of_bullet
-        self.end_of_last_bullet = end_of_last_bullet
-
 
 class TextVar(str):
     def __iadd__(self, other):
