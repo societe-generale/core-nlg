c236fa7c8a1ab575fa845c5ff4311949e50610c3
Fabien Giely <fabien.giely-ext@socgen.com>
2019-12-09 15:42:51 +0100

    add nlgtools functions

--- CoreNLG/NlgTools.py
+++ CoreNLG/NlgTools.py
@@ -5,16 +5,20 @@ created on 02/12/2019 16:03
 """
 import os
 
+from lxml import html
+from lxml.html import builder
+
 from CoreNLG.IterElems import IterElems
 from CoreNLG.KeyVals import KeyVals
 from CoreNLG.Synonym import Synonym
+from CoreNLG.decorators import beautifier
 from CoreNLG.resources.contraction import contraction
 
 from CoreNLG.AddTag import AddTag
 from CoreNLG.Logger import Logger
 from CoreNLG.NoInterpret import NoInterpret
 from CoreNLG.Number import Number
-from CoreNLG.tools import read_json_resource, get_resource_lang, read_default_words
+from CoreNLG.tools import read_json_resource, get_resource_lang, read_default_words, handle_string_to_html
 
 
 class NlgTools:
@@ -22,12 +26,14 @@ class NlgTools:
             self,
             html_elem="div",
             html_elem_attr=None,
-            active_printers=False,
             log_level="ERROR",
             lang="fr",
             freeze=False,
     ):
+        self._html = None
+        self.__html_elem_wrap(html_elem, html_elem_attr)
         self._lang = lang
+        self._is_beautiful = False
 
         self._log_level = log_level
         log = Logger(os.path.join(os.getcwd(), "logs"), self._log_level)
@@ -52,6 +58,43 @@ class NlgTools:
         self.post_evals = KeyVals()
         self.nlg_syn = Synonym(freeze, self.post_evals)
 
+    @property
+    def html(self):
+        """
+        Ca
+        :return:
+        """
+        self.__beautifier()
+        return self._html
+
+    @property
+    def html_str(self):
+        self.__beautifier()
+        return html.tostring(
+            self._html, pretty_print=True, encoding="utf-8"
+        ).decode("utf-8")
+
+    def write_text(self, *args, no_space=False):
+        """Creates a list of string"""
+        self._is_beautiful = False
+        text = list()
+        for arg in args:
+
+            arg = self.nlg_syn.handle_patterns(arg)
+            self.nlg_syn.update_position(arg)
+
+            text.append(arg)
+            if not no_space:
+                text.append(" ")
+
+        self.nlg_syn.smart_syno_lvl = 0
+        self.nlg_syn.synos_by_pattern = {}
+        span = handle_string_to_html(
+            builder.SPAN, "".join(text), builder.CLASS("to_delete")
+        )
+        self._html.append(span)
+        return span
+
     def __get_resources(self):
         resource_path = os.path.join(os.path.dirname(__file__), "resources")
         self._ponct = read_json_resource(os.path.join(resource_path, "ponctuation.json"),
@@ -63,3 +106,49 @@ class NlgTools:
                                                  self.logger)
 
         self._contract = get_resource_lang(contraction, self._lang, self.logger)
+
+    def __str__(self):
+        self.__beautifier()
+        return html.tostring(
+            self._html, pretty_print=True, encoding="utf-8"
+        ).decode("utf-8")
+
+    def __beautifier(self):
+        """
+
+        :return:
+        """
+        if not self._is_beautiful:
+            for span in self._html.xpath("//span[@class='to_delete']"):
+                span.drop_tag()
+            text = html.tostring(self._html, encoding="utf-8").decode("utf-8")
+            self._html = html.fromstring(
+                beautifier(text, self._ponct, self._contract)
+            )
+            self._is_beautiful = True
+        return self._html
+
+    def __html_elem_wrap(self, html_elem, html_elem_attr):
+        """
+        Creates a HTML container
+
+        :param html_elem: name of the HTML element (container) to be created
+        :type html_elem: string (eg. 'div')
+
+        :param html_elem_attr: indicates the attributes and their values, in the form {'class': 'a', ...}
+        :type html_elem_attr: dictionary (eg. { 'class': 'c', 'is': 'present' })
+
+        :return: an HTML element if a class is in the dictionary, a HTML builder if no class is in the dictionary
+        :rtype: if html_elem_attr['class'] exists: lxml.html.htmlElement; otherwise: lxml.html.builder
+        """
+        tag = getattr(builder, html_elem.upper())
+        if html_elem_attr is not None:
+            if "class" in html_elem_attr:
+                _class = builder.CLASS(html_elem_attr["class"])
+                html_elem_attr.pop("class")
+                tag = tag(_class, html_elem_attr)
+            else:
+                tag = tag(html_elem_attr)
+        else:
+            tag = tag()
+        self._html = tag
