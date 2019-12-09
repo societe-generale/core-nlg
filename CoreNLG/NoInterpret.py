20daa94065e3eec7aca75f9d76f36dbc308dcc47
Fabien Giely <fabien.giely-ext@socgen.com>
2019-12-09 11:03:46 +0100

    move constants out of class

--- CoreNLG/NoInterpret.py
+++ CoreNLG/NoInterpret.py
@@ -4,34 +4,38 @@ created on 02/12/2019 16:07
 @author: fgiely
 """
 from CoreNLG.AddTag import AddTag
-from CoreNLG.tools import take_second_arg_first_none
+from CoreNLG.tools import take_second_arg_if_first_none
+
+interpretable_char = {
+    ".": "DOT",
+    ",": "COMA",
+    '"': "QUOTE",
+    "?": "INTERROGATION",
+    "!": "EXCLAMATION",
+    ":": "DOUBLEDOTS",
+    "/": "SLASH",
+    " ": "SPACE",
+    ";": "SEMICOLON",
+    "&": "AMPERSAND",
+    "(": "OPENING_PARENTHESIS",
+    ")": "CLOSING_PARENTHESIS",
+    "<": "LESS_THAN",
+    ">": "MORE_THAN",
+    "[": "OPENING_BRACKET",
+    "]": "CLOSING_BRACKET",
+    "\n": "CARRIAGE_RETURN",
+}
+
+interpretable_char_reverse = {value: key for key, value in interpretable_char.items()}
 
 
 class NoInterpret:
-    interpretable_char = {
-        ".": "DOT",
-        ",": "COMA",
-        '"': "QUOTE",
-        "?": "INTERROGATION",
-        "!": "EXCLAMATION",
-        ":": "DOUBLEDOTS",
-        "/": "SLASH",
-        " ": "SPACE",
-        ";": "SEMICOLON",
-        "&": "AMPERSAND",
-        "(": "OPENING_PARENTHESIS",
-        ")": "CLOSING_PARENTHESIS",
-        "<": "LESS_THAN",
-        ">": "MORE_THAN",
-        "[": "OPENING_BRACKET",
-        "]": "CLOSING_BRACKET",
-        "\n": "CARRIAGE_RETURN",
-    }
+    interpretable_char = interpretable_char
 
-    interpretable_char_reverse = {value: key for key, value in interpretable_char.items()}
+    interpretable_char_reverse = interpretable_char_reverse
 
     def __init__(self, add_tag=None):
-        self.__add_tag = take_second_arg_first_none(add_tag, AddTag().add_tag)
+        self.__add_tag = take_second_arg_if_first_none(add_tag, AddTag().add_tag)
 
     def __no_interpret_char(self, char):
         try:
