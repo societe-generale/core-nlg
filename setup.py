8b2f28b44fffa3b7695a2fe1efcd959ab907efe1
Bastien Leca <bastien.leca-ext@socgen.com>
2020-01-02 16:50:47 +0100

    update setup.py for v2.04

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="CoreNLG",
-    version="2.0.3",
+    version="2.0.4",
     description="",
     long_description=long_description,
     long_description_content_type="text/markdown",
