2515d81128808c23ebcde2ecfc74f68eb5e403d4
Bastien Leca <bastien.leca-ext@socgen.com>
2020-01-02 16:49:37 +0100

    [synonyms] fix keyval activation for freeze or random mode

--- CoreNLG/KeyVals.py
+++ CoreNLG/KeyVals.py
@@ -12,13 +12,16 @@ class KeyVals:
         self.keyval_context = {}
 
     def post_eval(self, keyval, if_active='', if_inactive='', clean=False):
-        pattern = '~' + str(len(self.post_evals) + 1) + '~'
-        self.post_evals[pattern] = (keyval, if_active, if_inactive, clean)
-        return pattern
+        # in freeze or random mode, keyval has already be activated
+        if keyval in self.active_keyvals:
+            return self.handle_post_eval((keyval, if_active, if_inactive, clean))
+        else:
+            pattern = '~' + str(len(self.post_evals) + 1) + '~'
+            self.post_evals[pattern] = (keyval, if_active, if_inactive, clean)
+            return pattern
 
     def handle_post_eval(self, args):
         keyval, if_active, if_inactive, clean = args
         result = if_active if keyval in self.active_keyvals else if_inactive
         self.active_keyvals.remove(keyval) if clean and keyval in self.active_keyvals else None
         return result
-
