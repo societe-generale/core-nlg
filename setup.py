673bac9787d40d22ddc2f806c06df1e2900fc5fc
printfhere <55978226+printfhere@users.noreply.github.com>
2020-01-06 17:04:09 +0100

    update setup.py for v1.1.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="CoreNLG",
-    version="1.1.2",
+    version="1.1.3",
     description="",
     long_description=long_description,
     long_description_content_type="text/markdown",
