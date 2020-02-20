aea6aa18a1f3357884d7bd22792c5e89960b68be
Fabien <fabien.giely@addventa.com>
2020-02-20 16:40:06 +0100

    add tag doesn't uses html tags to allow more tags (for future xml implementation)

--- CoreNLG/AddTag.py
+++ CoreNLG/AddTag.py
@@ -4,44 +4,23 @@ created on 02/12/2019 16:08
 @author: fgiely
 """
 
-from lxml import html
-from lxml.html import builder
-
-from CoreNLG.tools import handle_string_to_html
-
 
 class AddTag:
-
     def add_tag(self, tag, text="", _class=None, **kwargs):
-        """
+        if tag is None:
+            return text
+        tag = tag.lower()
+        _class = "".join([" class=\"", _class, "\""]) if _class is not None else ""
+        attribs = ""
+        for k, v in kwargs.items():
+            attribs += " {}=\"{}\"".format(k, v)
 
-        :param tag: the HTML container name
-        :type tag: string
-
-        :param text:
-        :type text: string / list of strings
-
-        :param _class: the value of the attribute 'class' in the container
-        :type _class: string
-
-        :param kwargs:
-        :return:
-        """
-        try:
-            html_tag = getattr(builder, tag.upper())
-        except AttributeError:
-            return "tag {} doesn't exist in html".format(tag)
-        args = list()
-        if _class is not None:
-            args.append(builder.CLASS(_class))
         if isinstance(text, str):
-            e = handle_string_to_html(html_tag, text, *args, **kwargs)
-            return html.tostring(e, encoding="utf-8").decode("utf-8")
+            return "".join(["<", tag, _class, attribs, ">", text, "</", tag, ">"])
         else:
             iter_return = list()
             for t in text:
-                e = handle_string_to_html(html_tag, t, *args, **kwargs)
-                iter_return.append(html.tostring(e, encoding="utf-8").decode("utf-8"))
+                iter_return.append("".join(["<", tag, _class, attribs, ">", t, "</", tag, ">"]))
             return list(iter_return)
 
 
